36029ff408dec1d45b8f7104e7d23988f9fc83de
Svetoslav <veiv@mail.ru>
2023-07-16 17:29:04 +0300

    16 july

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const createBtn = document.getElementById('btn')
 const output = document.getElementById('output')
 const clearBtn = document.getElementById('clear')
 
-
+inputNum.value = ''
 // console.log(inputNum.value);
 createBtn.onclick = function () {
-    let toFaringeit = (9 / 5) * Number(inputNum.value) + 32 
-    console.log(toFaringeit);
-    toFaringeit += ' °F'
-    output.innerHTML = toFaringeit
+    if(inputNum.value !== '') {
+        let toFaringeit = (9 / 5) * Number(inputNum.value) + 32 
+        console.log(toFaringeit);
+        toFaringeit += ' °F'
+        output.innerHTML = toFaringeit
+    } else {
+        alert('Введите данные')
+    }
 }
 
 clearBtn.onclick = function (e) {
